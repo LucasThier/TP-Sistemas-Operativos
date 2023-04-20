b21fbb5dab3d447e5661b153c47cbe4a8212b7da
Lucas <lucasthier2015@gmail.com>
2023-04-20 00:14:31 +0000

    shared iniciar servidor

--- Shared/include/Sockets.h
+++ Shared/include/Sockets.h
@@ -5,6 +5,9 @@
 #include <commons/log.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <string.h>
 
 void Iniciar_Servidor(void);
 
--- Shared/src/Sockets.c
+++ Shared/src/Sockets.c
@@ -1,16 +1,56 @@
+#define _POSIX_C_SOURCE 200112L
+
 #include "../include/Sockets.h"
+
+
+void main(){
+    
+}
 
 void iniciar_servidor(char* ip, char* puerto)
 {
+    bool conectado;
     int socket_servidor;
-    struct addrinfo hints;
-    struct addrinfo  *servinfo;
+    struct addrinfo hints, *infoServer;
 
-    // Inicializando hints
-    memset(&hints, 0, sizeof(hints));
+    // setear las hints
+    memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
+    //buscar direccion del servidor siguiendo las hints
+    getaddrinfo(ip, puerto, &hints, &infoServer);
+    
+    //intentar conectar a cada direccion encontrada hasta
+    for (struct addrinfo *p = infoServer; p != NULL; p = p->ai_next)
+    {
+        if (socket(p->ai_family, p->ai_socktype, p->ai_protocol) == -1){
+            //LOGUEAR ERROR AL CREAR EL SOCKET
+            continue;
+        }
 
+        if (bind(socket_servidor, p->ai_addr, p->ai_addrlen) == -1)
+        {
+            //LOGUEAR ERROR EN EL BIND
+            close(socket_servidor);
+            continue;
+        }
+        conectado = true;
+        break;
+    }
+
+    if(true){
+
+    }
+
+    if(!conectado){
+
+        
+    }
+    
+    if(!conectado)
+    {
+
+    }
 }
