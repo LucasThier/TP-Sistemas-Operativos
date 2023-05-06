38c71c0c7739f8b4ca2d950c798c84813b29d586
Lucas <lucasthier2015@gmail.com>
2023-05-06 15:56:23 +0000

    estructura PCB

--- Kernel/include/KernelMain.h
+++ Kernel/include/KernelMain.h
@@ -21,6 +21,52 @@ char* PUERTO_CPU;
 char* PUERTO_ESCUCHA;
 void LeerConfigs(char* path);
 
+
+
+typedef struct
+{
+    char AX[5];
+    char BX[5];
+    char CX[5];
+    char DX[5];
+
+    char EAX[9];
+    char EBX[9];
+    char ECX[9];
+    char EDX[9];
+
+    char RAX[17];
+    char RBX[17];
+    char RCX[17];
+    char RDX[17];
+} t_registrosCPU;
+
+typedef struct
+{
+    int id;
+    int direccionBase;
+    int tamanoSegmento;
+} t_tablaSegmentos;
+
+typedef struct
+{
+    char* pathArchivoAbierto;
+    int posicionPuntero;
+
+} t_tablaArchivosAbiertos;
+
+typedef struct
+{
+    int PID;
+    void* listaInstrucciones;
+    int programCounter;
+    t_registrosCPU registrosCPU;
+    int estimacionProximaRafaga;
+    t_list* tablaDeSegmentos;
+    unsigned int tiempoLlegadaRedy;
+    t_list* tablaArchivosAbiertos;
+} t_PCB;
+
 char NOMBRE_PROCESO[7] = "KERNEL";
 
 #endif
