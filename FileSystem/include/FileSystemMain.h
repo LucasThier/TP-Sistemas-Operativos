732f3674fb2903dc17509f8bd93242f9d4153c0f
Lucas <lucasthier2015@gmail.com>
2023-07-01 00:04:30 +0000

    Funciones Bitmap, Archivo de bloques y FCBs

--- FileSystem/include/FileSystemMain.h
+++ FileSystem/include/FileSystemMain.h
@@ -6,21 +6,25 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <sys/mman.h>
+#include <dirent.h>
 
 
-typedef struct {
-	char* nombreArchivo;
-	int tamanoArchivo;
-	uint32_t* punteroDirecto;
-	uint32_t* punteroIndirecto;
-}FCB;
+t_log* FS_Logger;
+
+int SocketFileSystem;
+int SocketMemoria;
+int SocketKernel;
+
+pthread_t HiloEscucha;
 
 t_config *config, *configSB;
 char* IP_MEMORIA;
 char* PUERTO_MEMORIA;
 char* PUERTO_ESCUCHA;
 char* BLOCK_SIZE;
+int TAMANO_BLOQUES;
 char* BLOCK_COUNT;
+uint32_t CANTIDAD_BLOQUES;
 void* BLOQUES;
 char* PATH_FCB;
 char* PATH_BLOQUES;
@@ -33,5 +37,48 @@ void LiberarMemoria();
 void CrearArchivo(char*);
 
 char NOMBRE_PROCESO[11] = "FileSystem";
+
+
+//BITMAP:
+int ArchivoBitarray;
+char* bitarray;
+t_bitarray *bitmap;
+int InicializarBitmap();
+void ImprimirBitmap();
+bool BitmapEstaOcupado(uint32_t i);
+bool BitmapEstaLibre(uint32_t i);
+void BitmapOcuparBloque(uint32_t i);
+void BitmapLiberarBloque(uint32_t i);
+
+
+//BLOQUES
+int ArchivoBloques;
+char* MapArchivoBloques;
+int PunterosPorBloque;
+int InicializarArchivoDeBloques();
+char* ObtenerBloque(uint32_t IndiceBloque);
+void ImprimirBloque(uint32_t IndiceBloque, int Desplazamiento, int CantidadAImpimir);
+void EscribirBloqueDeChar(uint32_t IndiceBloque, int Desplazamiento, char* ContenidoAEscribir);
+char* LeerBloqueDeChar(uint32_t IndiceBloque, int Desplazamiento, int CantidadALeer);
+void EscribirBloqueDePunteros(uint32_t IndiceBloque, int IndicePuntero, uint32_t ContenidoAEscribir);
+uint32_t LeerBloqueDePunteros(uint32_t IndiceBloque, int IndicePuntero);
+
+
+//FCBs
+char* PathDirectorioFCBs = "PERSISTIDOS/FCB/";
+t_list* ListaFCBs;
+int InicializarFCBs();
+void CrearFCB(char* NombreArchivo, int TamanoArchivo, uint32_t Puntero_Directo, uint32_t Puntero_Indirecto);
+t_config* BuscarFCB(char* NombreArchivo);
+void ModificarValorFCB(t_config* FCB, char* KEY, char* Valor);
+
+
+
+//FUNCIONES DE FINALIZACION DEL MODULO
+void TerminarModulo();
+void PersistirDatos();
+void CerrarArchivoDeBloques();
+void CerrarArchivoDeBitmap();
+void CerrarFCBs();
 
 #endif
--- FileSystem/src/FileSystemMain.c
+++ FileSystem/src/FileSystemMain.c
@@ -1,39 +1,40 @@
 #include "../include/FileSystemMain.h"
 
-t_log* FS_Logger;
-
-int SocketFileSystem;
-int SocketMemoria;
-
-
 void sighandler(int s) {
-	liberar_conexion(SocketFileSystem);
+
+	TerminarModulo();
 	exit(0);
 }
 
 int main(int argc, char* argv[])
 {
 	signal(SIGINT, sighandler);
+	signal(SIGSEGV, sighandler);
 
 	FS_Logger = log_create("FileSystem.log", NOMBRE_PROCESO, true, LOG_LEVEL_INFO);
-
+	ListaFCBs = list_create();
 
 	//leer las config
 	LeerConfigs(argv[1],argv[2]);
 
+	if(InicializarBitmap() == 1)
+	{
+		return EXIT_FAILURE;
+	}
 
-	char* bitarray;
-	int cantBloques = atoi(BLOCK_COUNT);
-	int tamBloques = atoi(BLOCK_SIZE);
+	if(InicializarArchivoDeBloques() == 1)
+	{
+		return EXIT_FAILURE;
+	}
 
-	fd = open("Bloques.dat", O_RDWR, 0666);
-	BLOQUES = mmap(NULL, cantBloques*tamBloques, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if(InicializarFCBs() == 1)
+	{
+		return EXIT_FAILURE;
+	}
 
-	t_bitarray* bitmap[cantBloques];
-
-	for (int i = 0; i < cantBloques; i++) bitmap[i] = bitarray_create(bitarray, tamBloques);
-
-	//CrearArchivo("make");
+	CrearFCB("HolaQionda", 100, 41, 55);
+	CrearFCB("HolaQionda2", 101, 42, 56);
+	CrearFCB("HolaQionda3", 102, 43, 57);
 
 	InicializarConexiones();
 
@@ -49,8 +50,6 @@ int main(int argc, char* argv[])
 void InicializarConexiones()
 {
 	SocketMemoria = conectar_servidor(FS_Logger, "Memoria", IP_MEMORIA , PUERTO_MEMORIA);
-
-	pthread_t HiloEscucha;
 
     if (pthread_create(&HiloEscucha, NULL, EscuchaKernel, NULL) != 0) {
         exit(EXIT_FAILURE);
@@ -106,7 +105,8 @@ void* EscuchaKernel()
 		else if(strcmp(Pedido, "LEER_ARCHIVO") == 0)
 		{
 			char* NombreArchivo = strtok(NULL, " ");
-			char* DireccionFisica = strtok(NULL, " ");
+			char* NumSegmento = strtok(NULL, " ");
+			char* Offset = strtok(NULL, " ");
 			char* CantBytesALeer = strtok(NULL, " ");
 			char* Puntero = strtok(NULL, " ");
 
@@ -117,7 +117,8 @@ void* EscuchaKernel()
 		else if(strcmp(Pedido, "ESCRIBIR_ARCHIVO") == 0)
 		{
 			char* NombreArchivo = strtok(NULL, " ");
-			char* DireccionFisica = strtok(NULL, " ");
+			char* NumSegmento = strtok(NULL, " ");
+			char* Offset = strtok(NULL, " ");
 			char* CantBytesALeer = strtok(NULL, " ");
 			char* Puntero = strtok(NULL, " ");
 			
@@ -134,7 +135,6 @@ void LeerConfigs(char* path, char* path_superbloque)
 {
 	config = config_create(path);
 
-
 	IP_MEMORIA = config_get_string_value(config, "IP_MEMORIA");
 
 	PUERTO_MEMORIA = config_get_string_value(config, "PUERTO_MEMORIA");
@@ -149,35 +149,440 @@ void LeerConfigs(char* path, char* path_superbloque)
 	configSB = config_create(path_superbloque);
 
 	BLOCK_SIZE = config_get_string_value(configSB, "BLOCK_SIZE");
+	TAMANO_BLOQUES = atoi(BLOCK_SIZE);
 
 	BLOCK_COUNT = config_get_string_value(configSB, "BLOCK_COUNT");
+	CANTIDAD_BLOQUES = strtoul(BLOCK_COUNT, NULL, 10);
 
+	PunterosPorBloque = TAMANO_BLOQUES / sizeof(uint32_t);
 }
 
-void CrearArchivo(char* nombreArchivo){
-	FILE *archivo;
-	char* path_archivo= strcat(strcat(strcat(PATH_FCB,"/"),nombreArchivo),".dat");
 
-	archivo = fopen(path_archivo, "w");
-	if (archivo == NULL) {
-	        log_info(FS_Logger,"No se pudo crear el archivo.\n");
+
+
+//crea/abre el archivo, lo mapea, y crea el Bitarray usando las commons
+int InicializarBitmap()
+{
+	printf("Inicializando Bitmap\n");
+	char* PathArchivo = "PERSISTIDOS/Bitmap.dat";
+	bool ArchivoExiste = false;
+
+	//calcular el tamano en bytes del archivo
+	size_t CantidadBytes = CANTIDAD_BLOQUES / 8;
+    if (CANTIDAD_BLOQUES % 8 != 0)
+	{
+    	CantidadBytes++;
 	}
-	else{
-		log_info(FS_Logger,"Crear Archivo:%s.dat",nombreArchivo);
-	/*	char* key;
-		strcpy(key,"NOMBRE_ARCHIVO=");
-		char* nom=strcat(key,nombreArchivo);
-		fputs(nom,archivo);
-	*/
+
+	//Si el archivo SI existe
+	if(access(PathArchivo, F_OK) == 0)
+	{
+		ArchivoExiste = true;
 	}
 
-	fclose(archivo);
+	// Abrir/crear el archivo
+    ArchivoBitarray = open(PathArchivo, O_RDWR | O_CREAT, 0644);
+    if (ArchivoBitarray == -1) {
+        log_error(FS_Logger, "Error al abrir/crear el bitmap");
+        return 1;
+    }
 
+	// Truncar el archivo a la cantidad de bloques
+	if(ftruncate(ArchivoBitarray, CantidadBytes) == -1)
+	{
+		log_error(FS_Logger, "Error al truncar el bitmap");
+		return 1;
+	}
+
+	// Mapear el archivo a la memoria
+    bitarray = mmap(NULL, CantidadBytes, PROT_READ | PROT_WRITE, MAP_SHARED, ArchivoBitarray, 0);
+    if (bitarray == MAP_FAILED) {
+        perror("Error al mapear el archivo a la memoria");
+        close(ArchivoBitarray);
+        return 1;
+    }
+
+	// Crear el bitmap utilizando la biblioteca bitarray de las commons
+    bitmap = bitarray_create(bitarray, CantidadBytes);
+
+	// Si el archivo no existia, inicializar el bitmap en 0
+	if(!ArchivoExiste)
+	{
+		for(int i = 0; i < CANTIDAD_BLOQUES; i++)
+		{
+			bitarray_clean_bit(bitmap, i);
+		}
+		BitmapOcuparBloque(0);
+	}
+
+	return 0;
 }
-void TruncarArchivo(){
 
+//imprime el bitmap en pantalla
+void ImprimirBitmap()
+{
+	uint32_t inicioIntervalo = 0;
+	bool anterior = bitarray_test_bit(bitmap, 0);
+
+	for(uint32_t i = 1; i < CANTIDAD_BLOQUES; i++)
+	{
+		if(bitarray_test_bit(bitmap, i) != anterior)
+		{
+			printf("El intervalo [%d, %d] es %s\n", inicioIntervalo, i - 1, anterior ? "1" : "0");
+			anterior = !anterior;
+			inicioIntervalo = i;
+		}
+		if(i == CANTIDAD_BLOQUES - 1)
+		{
+			printf("El intervalo [%d, %d] es %s\n", inicioIntervalo, i, anterior ? "1" : "0");
+		}
+	}
 }
 
-void LiberarMemoria(){
-//	free(bitmap);
+//devuelve true si el bloque esta ocupado, false si esta libre
+bool BitmapEstaOcupado(uint32_t i)
+{
+	return bitarray_test_bit(bitmap, i);
+}
+
+//devuelve true si el bloque esta libre, false si esta ocupado
+bool BitmapEstaLibre(uint32_t i)
+{
+	return !bitarray_test_bit(bitmap, i);
+}
+
+//Marca como ocupado un bloque (lo pone en 1)
+void BitmapOcuparBloque(uint32_t i)
+{
+	bitarray_set_bit(bitmap, i);
+}
+
+//Marca como libre un bloque (lo pone en 0)
+void BitmapLiberarBloque(uint32_t i)
+{
+	bitarray_clean_bit(bitmap, i);
+}
+
+//retorna el "puntero" al bloque vacio
+uint32_t BitmapBuscarBloqueVacio()
+{
+	for(int i = 0; i < CANTIDAD_BLOQUES; i++)
+	{
+		if(BitmapEstaLibre(i))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+
+
+
+//crea/abre el archivo y lo mapea
+int InicializarArchivoDeBloques()
+{
+	printf("Inicializando archivo de bloques\n");
+	char* PathArchivo = "PERSISTIDOS/Bloques.dat";
+
+	//calcular el tamano en bytes del archivo
+	size_t CantidadBytes = CANTIDAD_BLOQUES * TAMANO_BLOQUES;
+
+	// Abrir/crear el archivo
+    ArchivoBloques = open(PathArchivo, O_RDWR | O_CREAT, 0644);
+    if (ArchivoBloques == -1) {
+        log_error(FS_Logger, "Error al abrir/crear el MapArchivoBloques");
+        return 1;
+    }
+
+	// Truncar el archivo a la cantidad de bloques
+	if(ftruncate(ArchivoBloques, CantidadBytes) == -1)
+	{
+		log_error(FS_Logger, "Error al truncar el MapArchivoBloques");
+		return 1;
+	}
+
+	// Mapear el archivo a la memoria
+    MapArchivoBloques = mmap(NULL, CantidadBytes, PROT_READ | PROT_WRITE, MAP_SHARED, ArchivoBloques, 0);
+    if (MapArchivoBloques == MAP_FAILED) {
+        perror("Error al mapear el archivo a la memoria");
+        close(ArchivoBloques);
+        return 1;
+    }
+	return 0;
+}
+
+//retorna un puntero al bloque i
+char* ObtenerBloque(uint32_t IndiceBloque)
+{
+	return MapArchivoBloques + (IndiceBloque * TAMANO_BLOQUES);
+}
+
+//imprime el contenido de N bytes de un bloque a partir de un determinado desplazamiento
+void ImprimirBloque(uint32_t IndiceBloque, int Desplazamiento, int CantidadAImpimir)
+{
+	if(Desplazamiento + CantidadAImpimir > TAMANO_BLOQUES)
+	{
+		log_error(FS_Logger, "Intentando imprimir mas alla del bloque");
+		return;
+	}
+	char* bloque = ObtenerBloque(IndiceBloque);
+	printf("El bloque %d contiene: %.*s\n", IndiceBloque, CantidadAImpimir, bloque + Desplazamiento);
+}
+
+//escribe el contenido de un CHAR en un bloque a partir de un determinado desplazamiento
+void EscribirBloqueDeChar(uint32_t IndiceBloque, int Desplazamiento, char* ContenidoAEscribir)
+{
+	if(strlen(ContenidoAEscribir) > TAMANO_BLOQUES - Desplazamiento)
+	{
+		log_error(FS_Logger, "Intentando escribir mas alla del final del bloque");
+		return;
+	}
+	char* bloque = ObtenerBloque(IndiceBloque);
+	memcpy(bloque, ContenidoAEscribir, strlen(ContenidoAEscribir));
+}
+
+//lee el contenido de N bytes de un bloque a partir de un determinado desplazamiento
+char* LeerBloqueDeChar(uint32_t IndiceBloque, int Desplazamiento, int CantidadALeer)
+{
+	if(Desplazamiento + CantidadALeer > TAMANO_BLOQUES)
+	{
+		log_error(FS_Logger, "Intentando leer mas alla del bloque");
+		return NULL;
+	}
+	char* bloque = ObtenerBloque(IndiceBloque);
+	char* contenido = malloc(CantidadALeer + 1);
+	memcpy(contenido, bloque + Desplazamiento, CantidadALeer);
+	contenido[CantidadALeer] = '\0';
+	return contenido;
+}
+
+//escribe un "puntero" (a un bloque) en un bloque a partir de un determinado desplazamiento
+void EscribirBloqueDePunteros(uint32_t IndiceBloque, int IndicePuntero, uint32_t ContenidoAEscribir)
+{
+	if((IndicePuntero + 1) * sizeof(uint32_t) > TAMANO_BLOQUES)
+	{
+		log_error(FS_Logger, "Intentando escribir mas alla del final del bloque");
+		return;
+	}
+
+	char* bloque = ObtenerBloque(IndiceBloque);
+	memcpy(bloque + (IndicePuntero * sizeof(uint32_t)), &ContenidoAEscribir, sizeof(uint32_t));
+}
+
+//retorna el "puntero" (a un bloque) en un bloque a partir de un determinado desplazamiento
+uint32_t LeerBloqueDePunteros(uint32_t IndiceBloque, int IndicePuntero)
+{
+	if((IndicePuntero + 1) * sizeof(uint32_t) > TAMANO_BLOQUES)
+	{
+		log_error(FS_Logger, "Intentando leer mas alla del bloque");
+		return 0;
+	}
+
+	char* bloque = ObtenerBloque(IndiceBloque);
+	uint32_t contenido;
+	memcpy(&contenido, bloque + (IndicePuntero * sizeof(uint32_t)), sizeof(uint32_t));
+	return contenido;
+}
+
+
+
+
+int InicializarFCBs()
+{
+	printf("Inicializando FCBs\n");
+
+	//abrir el config base para la creacion de nuevos FCBs
+	t_config* FCB = config_create("Base.FCB");
+	list_add(ListaFCBs, FCB);
+
+	//cargar todos los FCB que ya existen
+	DIR* dir;
+    struct dirent* entrada;
+
+    // Abrir el directorio
+    dir = opendir(PathDirectorioFCBs);
+    if (dir == NULL) {
+        printf("No se pudo abrir el directorio de FCBs.\n");
+        return 1;
+    }
+
+    // Leer las entradas del directorio
+    while ((entrada = readdir(dir)) != NULL) {
+        // Excluir las entradas "." y ".."
+        if (strcmp(entrada->d_name, ".") != 0 && strcmp(entrada->d_name, "..") != 0) {            
+			
+			//armar el path del archivo
+			char path[257];
+			sprintf(path, "%s/%s", PathDirectorioFCBs, entrada->d_name);
+
+			//crear el config para ese archivo y agregarlo a la lista
+			t_config* FCB = config_create(path);
+			list_add(ListaFCBs, FCB);
+        }
+    }
+
+    // Cerrar el directorio
+    closedir(dir);
+
+	return 0;
+}
+
+void CrearFCB(char* NombreArchivo, int TamanoArchivo, uint32_t Puntero_Directo, uint32_t Puntero_Indirecto)
+{
+	printf("Creando FCB para %s\n", NombreArchivo);
+
+	//armar el path del archivo
+	char path[100];
+	sprintf(path, "%s/%s.FCB", PathDirectorioFCBs, NombreArchivo);
+
+	//Si el FCB no existe
+	if(access(path, F_OK) != 0)
+	{
+		//buscar el FCB base
+		t_config* FCBAClonar = list_get(ListaFCBs, 0);
+
+		//clonar el config para ese archivo y agregarlo a la lista
+		config_save_in_file(FCBAClonar, path);
+		t_config* FCB = config_create(path);
+		list_add(ListaFCBs, FCB);
+
+		//setear los valores del FCB
+		config_set_value(FCB, "NOMBRE_ARCHIVO", NombreArchivo);
+		
+		char TamanoArchivoChar[15];
+		sprintf(TamanoArchivoChar, "%d", TamanoArchivo);
+		config_set_value(FCB, "TAMANIO_ARCHIVO", TamanoArchivoChar);
+
+		char PunteroDirectoChar[15];
+		sprintf(PunteroDirectoChar, "%u", Puntero_Directo);
+		config_set_value(FCB, "PUNTERO_DIRECTO", PunteroDirectoChar);
+
+		char PunteroIndirectoChar[15];
+		sprintf(PunteroIndirectoChar, "%u", Puntero_Indirecto);
+		config_set_value(FCB, "PUNTERO_INDIRECTO", PunteroIndirectoChar);
+
+		config_save(FCB);
+	}
+	else
+	{
+		log_error(FS_Logger, "El FCB para %s ya existe", NombreArchivo);
+	}
+}
+
+t_config* BuscarFCB(char* NombreArchivo)
+{
+	for(int i = 0; i < list_size(ListaFCBs); i++)
+	{
+		t_config* FCB = list_get(ListaFCBs, i);
+		if(strcmp(config_get_string_value(FCB, "NOMBRE_ARCHIVO"), NombreArchivo) == 0)
+			return FCB;
+	}
+	return NULL;
+}
+
+void ModificarValorFCB(t_config* FCB, char* KEY, char* Valor)
+{
+	config_set_value(FCB, KEY, Valor);
+	config_save(FCB);
+}
+
+
+
+
+
+
+
+//cierra y persiste el archivo de bitmap, desmapea la memoria y libera el bitmap
+void CerrarArchivoDeBitmap()
+{
+	printf("Cerrando Bitmap\n");
+
+	bitarray_destroy(bitmap);
+
+    // Sincronizar los datos pendientes en el bitmap
+    if (msync(bitarray, CANTIDAD_BLOQUES / 8, MS_SYNC) == -1) {
+        perror("Error al sincronizar el bitmap en la memoria");
+    }
+
+    // Desmapear el bitmap de la memoria
+    if (munmap(bitarray, CANTIDAD_BLOQUES / 8) == -1) {
+        perror("Error al desmapear el bitmap de la memoria");
+    }
+
+    // Sincronizar los datos pendientes en el archivo
+    if (fsync(ArchivoBitarray) == -1) {
+        perror("Error al sincronizar el archivo del bitmap");
+    }
+
+    // Cerrar el archivo
+    if (close(ArchivoBitarray) == -1) {
+        perror("Error al cerrar el archivo del bitmap");
+    }
+}
+
+//cierra y persiste el archivo de lloques y desmapea la memoria
+void CerrarArchivoDeBloques()
+{
+	printf("Cerrando archivo de bloques\n");
+
+	// Sincronizar los datos pendientes en el archivo
+	if (msync(MapArchivoBloques, CANTIDAD_BLOQUES * TAMANO_BLOQUES, MS_SYNC) == -1) {
+		perror("Error al sincronizar el MapArchivoBloques en la memoria");
+	}
+
+	// Desmapear el MapArchivoBloques de la memoria
+	if (munmap(MapArchivoBloques, CANTIDAD_BLOQUES * TAMANO_BLOQUES) == -1) {
+		perror("Error al desmapear el MapArchivoBloques de la memoria");
+	}
+
+	// Sincronizar los datos pendientes en el archivo
+	if (fsync(ArchivoBloques) == -1) {
+		perror("Error al sincronizar el archivo del MapArchivoBloques");
+	}
+
+	// Cerrar el archivo
+	if (close(ArchivoBloques) == -1) {
+		perror("Error al cerrar el archivo del MapArchivoBloques");
+	}
+}
+
+//limpia la lista que contiene los punteros a cada archivo FCB
+void CerrarFCBs()
+{
+	printf("Cerrando FCBs\n");
+
+	while(!list_is_empty(ListaFCBs))
+	{
+		t_config* FCB = list_remove(ListaFCBs, 0);
+		config_destroy(FCB);
+	}
+	list_destroy(ListaFCBs);
+}
+
+void PersistirDatos()
+{
+	printf("Persistiendo datos...\n");
+	CerrarArchivoDeBitmap();
+	CerrarArchivoDeBloques();
+	CerrarFCBs();
+}
+
+void TerminarModulo()
+{
+	printf("\nTerminando modulo...\n");
+
+	PersistirDatos();
+
+	pthread_cancel(HiloEscucha);
+	
+	config_destroy(config);
+	config_destroy(configSB);
+
+	log_destroy(FS_Logger);
+	
+	liberar_conexion(SocketKernel);
+	liberar_conexion(SocketMemoria);
+	liberar_conexion(SocketFileSystem);
 }
