9e0e229efd4fd3e946c3f1f4113d45481b5a4afb
Lucas Thier <lucasthier2015@gmail.com>
2023-08-05 14:53:01 -0300

    Update MemoriaMain.c

--- Memoria/src/MemoriaMain.c
+++ Memoria/src/MemoriaMain.c
@@ -445,7 +445,7 @@ log_info(Memoria_Logger,"segmento encontrado");
 		hueco->direccionBase = seg->direccionBase;
 		hueco->limite = seg->limite;
 		bool agregado=false;
-		verHuecos();
+		VerHuecos();
 		
 		for(int i = 0; i < list_size(TABLA_HUECOS); i++){
 			Hueco* h = list_get(TABLA_HUECOS,i);
@@ -457,7 +457,7 @@ log_info(Memoria_Logger,"segmento encontrado");
 				list_remove(TABLA_HUECOS,i);
 				list_add(TABLA_HUECOS,hueco);
 				agregado=true;
-				verHuecos();
+				VerHuecos();
 			}
 			else if(hueco->direccionBase + hueco->limite == h->direccionBase){
 				hueco->limite += h->limite;
@@ -466,7 +466,7 @@ log_info(Memoria_Logger,"segmento encontrado");
 				list_remove(TABLA_HUECOS,i);
 				list_add(TABLA_HUECOS,hueco);
 				agregado=true;
-				verHuecos();
+				VerHuecos();
 			}
 		}
 
